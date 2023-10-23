9a936ca0e258b906d3ccec201b0c175929b80321
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-23 11:16:24 +0200

    add: another solution (class correction)

--- js/script.js
+++ js/script.js
@@ -2,21 +2,33 @@ let gridString = "";
 
 const rowElem = document.querySelector(".row");
 
+const fizz = "Fizz";
+const buzz = "Buzz";
+
 for (let i = 1; i <= 100; i++){
     let result = "";
+    let className = "";
     if (i % 3 == 0 && i % 5 == 0){
-        result = "fizzbuzz";
-        gridString += `<div class = "square ${result}">${result}</div>`
+        // result = "FizzBuzz";
+        result = fizz+buzz;
+        className = "fizzbuzz";
+        // gridString += `<div class = "square ${result}">${result}</div>`
     } else if (i % 3 == 0 && !(i % 5 == 0)) {
-        result = "fizz";
-        gridString += `<div class = "square ${result}">${result}</div>`
+        // result = "Fizz";
+        result = fizz;
+        className = "fizz";
+        // gridString += `<div class = "square ${result}">${result}</div>`
     } else if (!(i % 3 == 0) && i % 5 == 0) {
-        result = "buzz";
-        gridString += `<div class = "square ${result}">${result}</div>`
+        // result = "Buzz";
+        result = buzz;
+        className = "buzz";
+        // gridString += `<div class = "square ${result}">${result}</div>`
     } else {
         result = i;
-        gridString += `<div class = "square">${i}</div>`
+        // gridString += `<div class = "square">${i}</div>`
     }
+
+    gridString += `<div class = "square ${className}">${result}</div>`
 
     console.log(result);
 
